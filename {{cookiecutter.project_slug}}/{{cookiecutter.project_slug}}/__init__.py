63bafdf29211bdcde4af64be081af544dcd3855c
Simon Dirmeier <simd23@protonmail.com>
2024-08-25 20:59:18 +0200

    Fix

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,5 +1,3 @@
-"""
-{{ cookiecutter.project_slug }}: {{ cookiecutter.project_short_description }}
-"""
+"""{{ cookiecutter.project_slug }}: {{ cookiecutter.project_short_description }}."""
 
 __version__ = "{{ cookiecutter.version }}"
